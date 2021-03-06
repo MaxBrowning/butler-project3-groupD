e954660ba72b5f2223d92b517beb4f2ee2db58ad
Cosette37 <69123930+Cosette37@users.noreply.github.com>
2021-03-06 09:42:06 -0600

    .

--- app.py
+++ app.py
@@ -29,6 +29,14 @@ def predict():
     if request.method == 'POST':
         Total_Assessed_Value = float(request.form['Assessed_Value'])
         Parcel_Acreage = float(request.form['Acreage'])
+        Vacant = request.form['Vacant']
+        if (Vacant == 'Yes'):
+            Parcel_Vacancy_Y = 1
+            Parcel_Vacancy_N = 0
+        else:
+            Parcel_Vacancy_Y = 0
+            Parcel_Vacancy_N = 1
+
         Property_Class = request.form['Property_Class']
 
         if (Property_Class == 'Agricultural'):
@@ -37,7 +45,6 @@ def predict():
             Class_Code_Translated_Exempt = 0 
             Class_Code_Translated_Industrial = 0 
             Class_Code_Translated_Locally_Assessed = 0 
-            Class_Code_Translated_Mineral = 0 
             Class_Code_Translated_Residential = 0 
         elif (Property_Class =='Commercial'):
             Class_Code_Translated_Agricultural = 0
@@ -45,15 +52,13 @@ def predict():
             Class_Code_Translated_Exempt = 0 
             Class_Code_Translated_Industrial = 0 
             Class_Code_Translated_Locally_Assessed = 0 
-            Class_Code_Translated_Mineral = 0 
             Class_Code_Translated_Residential = 0 
         elif (Property_Class == 'Exempt'):
             Class_Code_Translated_Agricultural = 0
             Class_Code_Translated_Commercial = 0
             Class_Code_Translated_Exempt = 1 
             Class_Code_Translated_Industrial = 0 
-            Class_Code_Translated_Locally_Assessed = 0  
-            Class_Code_Translated_Mineral = 0 
+            Class_Code_Translated_Locally_Assessed = 0   
             Class_Code_Translated_Residential = 0 
         elif (Property_Class == 'Industrial'):
             Class_Code_Translated_Agricultural = 0
@@ -61,35 +66,28 @@ def predict():
             Class_Code_Translated_Exempt = 0 
             Class_Code_Translated_Industrial = 1 
             Class_Code_Translated_Locally_Assessed = 0 
-            Class_Code_Translated_Mineral = 0 
             Class_Code_Translated_Residential = 0 
+        elif (Property_Class == 'Locally Assessed'):
+            Class_Code_Translated_Agricultural = 0
+            Class_Code_Translated_Commercial = 0
+            Class_Code_Translated_Exempt = 0 
+            Class_Code_Translated_Industrial = 0 
+            Class_Code_Translated_Locally_Assessed = 1  
+            Class_Code_Translated_Residential = 0
         elif (Property_Class == 'Residential'):
             Class_Code_Translated_Agricultural = 0
             Class_Code_Translated_Commercial = 0
             Class_Code_Translated_Exempt = 0 
             Class_Code_Translated_Industrial = 0 
-            Class_Code_Translated_Locally_Assessed = 0 
-            Class_Code_Translated_Mineral = 0 
+            Class_Code_Translated_Locally_Assessed = 0  
             Class_Code_Translated_Residential = 1
-        else:
-            Class_Code_Translated_Agricultural = 0
-            Class_Code_Translated_Commercial = 0
-            Class_Code_Translated_Exempt = 0 
-            Class_Code_Translated_Industrial = 0 
-            Class_Code_Translated_Locally_Assessed = 0 
-            Class_Code_Translated_Mineral = 0 
-            Class_Code_Translated_Residential = 0  
-
-        Vacant = request.form['Vacant']
-        if (Vacant == 'Yes'):
-            Vacant = 1
-        else:
-            Vacant = 0
-
-        prediction = model.predict([[Total_Assessed_Value, Parcel_Acreage, Class_Code_Translated_Agricultural, Class_Code_Translated_Commercial, Class_Code_Translated_Exempt, Class_Code_Translated_Industrial, Class_Code_Translated_Locally_Assessed, Class_Code_Translated_Mineral, Class_Code_Translated_Residential, Vacant]])
+# open file pickle.load x and y
+# apply x scaler to xdata
+        prediction = model.predict([[Total_Assessed_Value, Parcel_Acreage, Parcel_Vacancy_Y, Parcel_Vacancy_N, Class_Code_Translated_Agricultural, Class_Code_Translated_Commercial, Class_Code_Translated_Exempt, Class_Code_Translated_Industrial, Class_Code_Translated_Locally_Assessed, Class_Code_Translated_Residential]])
+# apply y scaler.inverse transform
         output = round(prediction[0],2)
 
-        print([[Total_Assessed_Value, Parcel_Acreage, Class_Code_Translated_Agricultural, Class_Code_Translated_Commercial, Class_Code_Translated_Exempt, Class_Code_Translated_Industrial, Class_Code_Translated_Locally_Assessed, Class_Code_Translated_Mineral, Class_Code_Translated_Residential, Vacant]])
+        print([[Total_Assessed_Value, Parcel_Acreage, Parcel_Vacancy_Y, Parcel_Vacancy_N, Class_Code_Translated_Agricultural, Class_Code_Translated_Commercial, Class_Code_Translated_Exempt, Class_Code_Translated_Industrial, Class_Code_Translated_Locally_Assessed, Class_Code_Translated_Residential]])
 
 
 
