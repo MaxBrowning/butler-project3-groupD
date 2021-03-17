8c52b675c482da0e4414555537e9cafa9488475e
MaxBrowning <browning.max@gmail.com>
2021-03-17 18:47:33 -0400

    added port to app.py

--- app.py
+++ app.py
@@ -125,5 +125,5 @@ def comps():
 
 if __name__ == '__main__':
     app.run(debug=True)
-    #port = int(os.environ.get("PORT", 5000))
-    #app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
