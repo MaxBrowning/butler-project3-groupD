28e9c0a180b9a4d40bf8a4f7bb86e218505e0c4b
MaxBrowning <browning.max@gmail.com>
2021-03-17 19:13:24 -0400

    bug fix

--- app.py
+++ app.py
@@ -123,6 +123,6 @@ def comps():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
